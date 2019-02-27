8b4c06b94eda96b0789b45685116937326be0f98
sigman <wojciech.sierakowski@hmhco.com>
2019-02-27 22:47:36 +0000

    test: add test coverage reports

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   // setupFiles: ['../test/unit/setup-jest-unit.js'],
+  coverageDirectory: '<rootDir>/coverage',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
   testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
   testPathIgnorePatterns: [
     '<rootDir>/node_modules',
